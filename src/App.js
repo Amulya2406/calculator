9eedf7d839c752b3c23ff1c188a845e92f18ffc0
Amulya2406 <amulyanayakd24@gmail.com>
2023-07-20 18:41:07 +0530

    deployment commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import * as math from 'mathjs';
 
 function App() {
+  const [input, setInput] = useState('');
+
+  const handleButtonPress = (value) => {
+    setInput(input + value);
+  };
+
+  const handleClear = () => {
+    setInput('');
+  };
+
+  const handleClearLastCharacter = () => {
+    const operators = ['+', '-', '*', '/'];
+    const lastChar = input.slice(-1);
+    if (operators.includes(lastChar)) {
+      setInput(input.slice(0, -1));}
+    setInput(input.slice(0, -1));
+  };
+
+  
+  const handleCalculate = () => {
+    try {
+      const result = math.evaluate(input);
+      setInput(result.toString());
+    } catch (error) {
+      setInput('Error');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div className="calculator-container">
+      <h1>Calculator</h1>
+    
+    <div className="calculator">
+      <div className="input">{input}</div>
+      <div className="buttons">
+        <button onClick={() => handleButtonPress('7')}>7</button>
+        <button onClick={() => handleButtonPress('8')}>8</button>
+        <button onClick={() => handleButtonPress('9')}>9</button>
+        <button onClick={() => handleButtonPress('+')}>+</button>
+        <button onClick={() => handleButtonPress('4')}>4</button>
+        <button onClick={() => handleButtonPress('5')}>5</button>
+        <button onClick={() => handleButtonPress('6')}>6</button>
+        <button onClick={() => handleButtonPress('-')}>-</button>
+        <button onClick={() => handleButtonPress('1')}>1</button>
+        <button onClick={() => handleButtonPress('2')}>2</button>
+        <button onClick={() => handleButtonPress('3')}>3</button>
+        <button onClick={() => handleButtonPress('*')}>*</button>
+        <button onClick={() => handleButtonPress('0')}>0</button>
+        <button onClick={() => handleButtonPress('.')}>.</button>
+        <button onClick={() => handleButtonPress('/')}>/</button>
+        <button onClick={handleClearLastCharacter}>C</button>
+        
+        <button onClick={handleClear} className="clear">Clear</button>
+        <button onClick={handleCalculate} className="equal">=</button>
+      </div>
     </div>
+  
+    </div>
+
   );
 }
 
